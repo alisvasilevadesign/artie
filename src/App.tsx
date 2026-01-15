47e71c6283de1014367684b1c4f1b0ab33a71ccf
alisvasilevadesign <alisvasileva.design@gmail.com>
2026-01-15 21:55:22 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { BookmarkProvider } from "@/context/BookmarkContext";
+
 import Index from "./pages/Index";
 import Gallery from "./pages/Gallery";
 import Artists from "./pages/Artists";
@@ -23,20 +24,20 @@ const App = () => (
       <BookmarkProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/gallery" element={<Gallery />} />
-            <Route path="/artists" element={<Artists />} />
-            <Route path="/artist/:id" element={<ArtistDetail />} />
-            <Route path="/artwork/:id" element={<ArtworkDetail />} />
-            <Route path="/timeline" element={<Timeline />} />
-            <Route path="/styles" element={<Styles />} />
-            <Route path="/quiz" element={<Quiz />} />
-            <Route path="/bookmarks" element={<Bookmarks />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/artists" element={<Artists />} />
+          <Route path="/artist/:id" element={<ArtistDetail />} />
+          <Route path="/artwork/:id" element={<ArtworkDetail />} />
+          <Route path="/timeline" element={<Timeline />} />
+          <Route path="/styles" element={<Styles />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/bookmarks" element={<Bookmarks />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
       </BookmarkProvider>
     </TooltipProvider>
   </QueryClientProvider>
